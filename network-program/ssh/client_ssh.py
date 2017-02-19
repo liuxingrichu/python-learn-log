aa12207bffd845e9842d57c45123fa925f19e849
liuxingrichu <liuxingrichu@163.com>
2017-02-19 16:10:47 +0800

    【新增1】服务器异常捕获 【新增2】粘包处理的三种方式 【新增3】处理大数据传输的接收

--- network-program/ssh/client_ssh.py
+++ network-program/ssh/client_ssh.py
@@ -13,7 +13,34 @@ while True:
         continue
     client.send(cmd.encode())
 
-    data = client.recv(1024)
-    print(data.decode())
+    cmd_res_size = client.recv(1024)
+
+    if cmd_res_size.decode() == '400':
+        print("\t\tno data")
+        continue
+
+    # method2: avoid stick package
+    client.send("可以接受数据了".encode())
+
+    cmd_res_size = int(cmd_res_size.decode())
+    received_size = 0
+    received_data = b''
+
+    while received_size < cmd_res_size:
+        remainder_size = cmd_res_size - received_size
+        if remainder_size > 1024:
+            data = client.recv(1024)
+            received_data += data
+            received_size += 1024
+        else:
+            # method3: avoid stick package
+            data = client.recv(remainder_size)
+            received_data += data
+            received_size += remainder_size
+    else:
+        print("\t\tdata receive done")
+
+    print("recv size: ", received_size)
+    print(received_data.decode())
 
 client.close()
--- network-program/ssh/server_ssh.py
+++ network-program/ssh/server_ssh.py
@@ -3,23 +3,41 @@
 
 import socket
 import os
+import time
 
 server = socket.socket()
 
-server.bind(('localhost', 9999))
+server.bind(('0.0.0.0', 9999))
 server.listen()
 
 while True:
     conn, addr = server.accept()
     while True:
-        data = conn.recv(1024)
-        if not data:
-            print("client is closed.")
+        try:
+            data = conn.recv(1024)
+        except ConnectionResetError:
+            print("\t\tclient is closed.")
             break
+
         cmd_res = os.popen(data.decode()).read()
+
         if len(cmd_res) == 0:
-            conn.send("no data".encode())
-        else:
-            conn.send(cmd_res.encode())
+            # send must not empty, 400：no data
+            conn.send("400".encode())
+            continue
+        """
+        send cmd_res size
+        assure server send data size and client receive data size same
+        must encode cmd_res, especially, Chinese character.
+        """
+        cmd_size = conn.send(str(len(cmd_res.encode())).encode())
+
+        # method1: avoid stick package(不推荐)
+        # time.sleep(0.5)
+
+        # method2: avoid stick package
+        conn.recv(1024)
+
+        conn.send(cmd_res.encode())
 
 server.close()
